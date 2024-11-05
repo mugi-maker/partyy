fcf03293f44a433e918cfc20e58b31f0ff728515
mugi-maker <chatgptchatgpt027@gmail.com>
2024-11-05 23:32:37 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,23 @@
-import App from './Layout.svelte'
+import Loader from './Layout.svelte'
+import App from './fotter.svelte'
+import Baby from './baby.svelte'
+import Scott from './scott.svelte'
+
 
 const app = new App({
   target: document.getElementById('app')
 })
 
-export default app
+ export default app
+
+ const apps = new Loader({
+  target: document.getElementById('app2')
+ })
+
+ const app0 = new Baby({
+  target: document.getElementById('app0')
+ })
+
+ const app3 = new Scott({
+  target: document.getElementById('app3')
+ })
